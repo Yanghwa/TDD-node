cb191fced8e2a2a6517a665fed0f4a1f16d5036f
Yanghwa <kellerj87@hotmail.com>
2018-10-01 14:46:15 -0400

    test spec seperate

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const bodyParser = require('body-parser');
 const user = require('./api/user');
 const app = express();
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV !== 'test') {
+    app.use(morgan('dev'));
+} 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', user);
-
-app.listen(3000, () => {console.log('server is running');});
 
 module.exports = app;
